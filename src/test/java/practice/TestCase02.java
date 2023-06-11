15d22c55c15f6ac6c6fbceffb4513f81b09867bb
sumrabia <sumrabiasahin>
2023-06-11 21:08:12 +0300

    jUnit

--- src/test/java/practice/TestCase02.java
+++ src/test/java/practice/TestCase02.java
@@ -14,24 +14,38 @@ public class TestCase02 {
 
         System.setProperty("webdriver.http.factory", "jdk-http-client");
         WebDriverManager.chromedriver().setup();
-        WebDriver driver= new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.get("https://www.facebook.com/");
 
 
         //email kismina random bir mail adresi giriniz.
-        WebElement emailButton=driver.findElement(By.id("email"));
+        WebElement emailButton = driver.findElement(By.id("email"));
         emailButton.click();
         emailButton.sendKeys("sumrabiasahin@gmail.com");
 
         //password kismina random bir password adresi giriniz.
-        WebElement passwordButton=driver.findElement(By.xpath("//input[@type='password']"));
+        WebElement passwordButton = driver.findElement(By.xpath("//input[@type='password']"));
         passwordButton.click();
         passwordButton.sendKeys("4286Sumeyye." + Keys.ENTER);
 
+        //uyari mesajini dondur
+        WebElement failedMessage = driver.findElement(By.xpath("//div[@class='_9ay7']"));
+        System.out.println(failedMessage.getText());
+        String expectedResult = "The email address you entered isn't connected to an account. Find your account and log in.";
 
+        if (failedMessage.getText().contains(expectedResult)) {
+            System.out.println("The failed message that appears and the expected result are the same.");
+        } else {
+            System.out.println("the failed message that appears ant the expected result are not the same." +
+                    "Test case did not pass.");
+        }
 
-
+        //sayfayi kapatin.
+        driver.close();
     }
+
+
 }
+
